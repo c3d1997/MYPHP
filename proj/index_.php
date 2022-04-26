e1f2c96334615c956fb617405e26803d5dbb8c96
c3d1997 <c3d1997@gmail.com>
2022-04-26 17:25:12 +0800

    20220426

--- proj/index_.php
+++ proj/index_.php
@@ -1,5 +1,12 @@
-<?php include __DIR__ . '/parts/html-head.php' ?>
-<?php include __DIR__ . '/parts/navbar.php' ?>
-<div class="container"></div>
-<?php include __DIR__ . '/parts/scripts.php' ?>
-<?php include __DIR__ . '/parts/html-foot.php' ?>
+<php? 
+$pageName='index';
+$title='我的首頁';
+?>
+    <?php include __DIR__ . '/parts/html-head.php' ?>
+    <?php include __DIR__ . '/parts/navbar.php' ?>
+    <div class="container">
+        <h2>Hello</h2>
+    </div>
+    <?php include __DIR__ . '/parts/scripts.php' ?>
+    <script></script>
+    <?php include __DIR__ . '/parts/html-foot.php' ?>
